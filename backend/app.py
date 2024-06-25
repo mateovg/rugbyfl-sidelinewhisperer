650ba6ac44292a85320ac72dca0c55485a5d8faa
Mateo Velazquez <mvela092@fiu.edu>
2024-06-25 12:49:22 -0400

    removed frontend tests for now

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,6 @@ def hello():
 
 @app.route('/api/matches', methods=['GET'])
 def get_matches():
-    # This is a placeholder. In the future, you'll fetch this from a database
     matches = [
         {"id": 1, "home_team": "Miami RFC",
             "away_team": "Orlando RFC", "date": "2023-07-01"},
