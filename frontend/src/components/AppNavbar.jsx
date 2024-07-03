2b4114e0cc5db8d91bf9c5c3d5c9c47c55d05e56
Mateo Velazquez <mvela092@fiu.edu>
2024-07-03 11:00:32 -0400

    Deleted old frontend

--- frontend/src/components/AppNavbar.jsx
+++ frontend/src/components/AppNavbar.jsx
@@ -1,18 +1,34 @@
+import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
 function AppNavbar() {
   return (
-    <>
-      <Navbar bg="primary" data-bs-theme="dark" fixed="top">
-        <Container>
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
+          <img
+            alt=""
+            src="../assets/react.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Brand Link
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/matches">Matches</Nav.Link>
+          </Nav>
+          <Nav>
             <Nav.Link href="/leaderboard">Leaderboard</Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
-    </>
+          <Nav>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
