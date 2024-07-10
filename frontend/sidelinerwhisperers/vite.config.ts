e914f1380e24614011cafb2a52fcaaad52770bf0
Mateo Velazquez <mateovg@gmail.com>
2024-07-10 10:05:49 -0400

    Starting over again

--- frontend/sidelinerwhisperers/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import { builderDevTools } from "@builder.io/dev-tools/vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-
-export default defineConfig({
-  plugins: [react(), builderDevTools()],
-});
