e2f6f40a801c942963d987270eb8a14319a91b31
Mateo Velazquez <mateovg@gmail.com>
2024-07-10 10:18:33 -0400

    Refactor config.py

--- predictor-api-service/config.py
+++ predictor-api-service/config.py
@@ -1,8 +1,21 @@
 class Config:
-    DEBUG = True
-    TESTING = False
     # more config options: ei database connection strings, etc
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
 
 
 class TestingConfig(Config):
     TESTING = True
+
+
+confg = {
+    'development': 'DevelopmentConfig',
+    'testing': 'TestingConfig',
+    'default': 'DevelopmentConfig'
+}
